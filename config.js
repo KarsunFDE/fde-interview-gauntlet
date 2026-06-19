51f83caa36a3e9e60077a922de36b415d75216f2
JesterCharles <jestercharles@gmail.com>
2026-06-19 13:36:06 -0400

    config: wire deployed Worker URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Runtime config for FDE Interview Gauntlet.
 // Edit WORKER_URL after deploying the Cloudflare Worker backend.
 window.CONFIG = {
-  WORKER_URL: "REPLACE_WITH_WORKER_URL",
+  WORKER_URL: "https://fde-interview-gauntlet.jestercharles.workers.dev",
   ROSTER: [
     "Kevin Sung",
     "Aaron Grau",
